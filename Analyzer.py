e800d74af1dfa9c5a7ea1114823ca898d588b639
bradziolko <t.m.m.xiv@gmail.com>
2016-06-15 20:11:21 -0500

    Refactored all of the word analyzation and confidence to its own class.

---
+++ Analyzer.py
@@ -0,0 +1,146 @@
+
+
+class Analyzer:
+    def __init__(self):
+        return
+    
+    def findThenThanIndex(self, words, thenorthan):
+        return words.index(thenorthan)
+    
+    def findWord(self, words, word, index, thenthanIndex):
+        if (self.checkRange(words, index, thenthanIndex)):
+            if (words[index + thenthanIndex] == word):
+                return True
+            
+        return False
+    
+    def checkRange(self, words, index, thenthanIndex):
+        if (index + thenthanIndex) >= len(words):
+            return False
+        elif (index + thenthanIndex) < 0:
+            return False
+        
+        return True
+
+    def confidenceWeight(self, index):
+        index = abs(index)
+        
+        if (index == 1):
+            return 1
+        elif (index == 2):
+            return .66
+        elif (index == 3):
+            return .33
+
+    def getThenConfidence(self, then, than, words, thenthanIndex):
+        confidence = 0
+        
+        i = 0
+        for t in than:
+            index = t[1]
+            word = t[0]
+            
+            if (self.findWord(words, word, index, thenthanIndex)):
+                confidence = (100 - (i * 10)) * self.confidenceWeight(index)
+                
+            i += 1
+        
+        if (confidence > 0):
+            i = 0
+            for t in then:
+                index = t[1]
+                word = t[0]
+                
+                if (self.findWord(words, word, index, thenthanIndex)):
+                    confidence = confidence - ((100 -(i * 10)) * self.confidenceWeight(index))
+                i += 1
+                
+        return confidence
+
+    def getThanConfidence(self, then, than, words, thenthanIndex):
+        confidence = 0
+        
+        i = 0
+        for t in then:
+            index = t[1]
+            word = t[0]
+                
+            if (self.findWord(words, word, index, thenthanIndex)):
+                confidence = (100 - (i * 10)) * self.confidenceWeight(index)
+            i += 1
+                
+        if (confidence > 0):
+            i = 0
+            for t in than:
+                index = t[1]
+                word = t[0]
+            
+                if (self.findWord(words, word, index, thenthanIndex)):
+                    confidence = confidence - ((100 - (i * 10)) * self.confidenceWeight(index))
+                i += 1
+                    
+        return confidence
+    
+    def processWordsForDB(self, sql, reddit, comment, words, thenorthan):
+        sql.newView(comment[1])
+        thenthanIndex = self.findThenThanIndex(words, thenorthan)
+        index = 0
+        
+        for word in words:
+            
+            if (word == "the" or len(word) < 3 or len(word) > 15):
+                continue
+            
+            if (thenorthan == "then"):
+                exists = sql.thenExists(word, index - thenthanIndex)
+                
+                if (index - thenthanIndex <= 3 and index - thenthanIndex >= -3):
+                    if (exists):
+                        sql.updateThen(word, index - thenthanIndex)
+                    else:
+                        sql.newThen(word, index - thenthanIndex)
+            else:
+                exists = sql.thanExists(word, index - thenthanIndex)
+                
+                if (index - thenthanIndex <= 3 and index - thenthanIndex >= -3):
+                    if (exists):
+                        sql.updateThan(word, index - thenthanIndex)
+                    else:
+                        sql.newThan(word, index - thenthanIndex)
+        
+            index += 1
+    
+    def analyze(self, sql, reddit, comment, words, thenorthan):
+        commented = sql.getComments()
+        
+        if (comment[1] in commented):
+            return False
+        
+        thenthanIndex = self.findThenThanIndex(words, thenorthan)
+        threshold = sql.getConfidence()
+        thresholdN = threshold[1]
+        threshold = threshold[0]
+        then = sql.getTopThens()
+        than = sql.getTopThans()
+        
+        if (thenorthan == "then"):
+            confidence = self.getThenConfidence(then, than, words, thenthanIndex)
+        elif (thenorthan == "than"):
+            confidence = self.getThanConfidence(then, than, words, thenthanIndex)
+            
+        if (confidence > threshold / thresholdN):
+            print "-----------------------------------------------"
+            print "                  COMMENTING!                  "
+            print "-----------------------------------------------"
+            print "Confidence:"
+            print confidence
+            if (thresholdN != 0):
+                print "Threshold:"
+                print threshold/thresholdN
+            print "Comment Text:\n"
+            print comment[0]
+    
+            reddit.postComment(comment[1])
+            sql.newComment(comment[1], comment[0], comment[2])
+        
+        return True    
--- main.py
+++ main.py
@@ -1,158 +1,17 @@
 from SQLHandler import SQLHandler
 from Reddit import Reddit
+from Analyzer import Analyzer
 import time
-
-def findThenThanIndex(words, thenorthan):
-    return words.index(thenorthan)
 
 def thenOrThan(words):
     return "then" if "then" in words else "than"
-
-def checkRange(words, index, thenthanIndex):
-    if (index + thenthanIndex) >= len(words):
-        return False
-    elif (index + thenthanIndex) < 0:
-        return False
-    
-    return True
-
-def findWord(words, word, index, thenthanIndex):
-    if (checkRange(words, index, thenthanIndex)):
-        if (words[index + thenthanIndex] == word):
-            return True
-        
-    return False
-
-def confidenceWeight(index):
-    index = abs(index)
-    
-    if (index == 1):
-        return 1
-    elif (index == 2):
-        return .66
-    elif (index == 3):
-        return .33
-    
-
-def getThenConfidence(then, than, words, thenthanIndex):
-    confidence = 0
-    
-    i = 0
-    for t in than:
-        index = t[1]
-        word = t[0]
-        
-        if (findWord(words, word, index, thenthanIndex)):
-            confidence = (100 - (i * 10)) * confidenceWeight(index)
-            
-        i += 1
-    
-    if (confidence > 0):
-        i = 0
-        for t in then:
-            index = t[1]
-            word = t[0]
-            
-            if (findWord(words, word, index, thenthanIndex)):
-                confidence = confidence - ((100 -(i * 10)) * confidenceWeight(index))
-            i += 1
-            
-    return confidence
-
-def getThanConfidence(then, than, words, thenthanIndex):
-    confidence = 0
-    
-    i = 0
-    for t in then:
-        index = t[1]
-        word = t[0]
-            
-        if (findWord(words, word, index, thenthanIndex)):
-            confidence = (100 - (i * 10)) * confidenceWeight(index)
-        i += 1
-            
-    if (confidence > 0):
-        i = 0
-        for t in than:
-            index = t[1]
-            word = t[0]
-        
-            if (findWord(words, word, index, thenthanIndex)):
-                confidence = confidence - ((100 - (i * 10)) * confidenceWeight(index))
-            i += 1
-                
-    return confidence
-
-def analyze(sql, reddit, comment, words, thenorthan):
-    commented = sql.getComments()
-    
-    if (comment[1] in commented):
-        return False
-    
-    thenthanIndex = findThenThanIndex(words, thenorthan)
-    threshold = sql.getConfidence()
-    thresholdN = threshold[1]
-    threshold = threshold[0]
-    then = sql.getTopThens()
-    than = sql.getTopThans()
-    
-    if (thenorthan == "then"):
-        confidence = getThenConfidence(then, than, words, thenthanIndex)
-    elif (thenorthan == "than"):
-        confidence = getThanConfidence(then, than, words, thenthanIndex)
-        
-    if (confidence > threshold / thresholdN):
-        print "-----------------------------------------------"
-        print "                  COMMENTING!                  "
-        print "-----------------------------------------------"
-        print "Confidence:"
-        print confidence
-        if (thresholdN != 0):
-            print "Threshold:"
-            print threshold/thresholdN
-        print "Comment Text:\n"
-        print comment[0]
-
-        reddit.postComment(comment[1])
-        sql.newComment(comment[1], comment[0], comment[2])
-    
-    return True
-
-def process(sql, reddit, comment, words, thenorthan):
-    sql.newView(comment[1])
-    thenthanIndex = findThenThanIndex(words, thenorthan)
-    index = 0
-    
-    for word in words:
-        
-        if (word == "the" or len(word) < 3 or len(word) > 15):
-            continue
-        
-        if (thenorthan == "then"):
-            exists = sql.thenExists(word, index - thenthanIndex)
-            
-            if (index - thenthanIndex <= 3 and index - thenthanIndex >= -3):
-                if (exists):
-                    sql.updateThen(word, index - thenthanIndex)
-                else:
-                    sql.newThen(word, index - thenthanIndex)
-        else:
-            exists = sql.thanExists(word, index - thenthanIndex)
-            
-            if (index - thenthanIndex <= 3 and index - thenthanIndex >= -3):
-                if (exists):
-                    sql.updateThan(word, index - thenthanIndex)
-                else:
-                    sql.newThan(word, index - thenthanIndex)
-    
-        index += 1
-                
 
 count = 0
 
 while True:
     sql = SQLHandler()
     reddit = Reddit()
+    analyzer = Analyzer()
     
     count += 1
     
@@ -174,10 +33,10 @@ while True:
                 break
         
         if (flag):
-            flag = analyze(sql, reddit, comment, words, thenorthan)
+            flag = analyzer.analyze(sql, reddit, comment, words, thenorthan)
         
         if (flag):
-            process(sql, reddit, comment, words, thenorthan)
+            analyzer.processWordsForDB(sql, reddit, comment, words, thenorthan)
 
     print "Sleeping for 1 minute"
     time.sleep(60)
